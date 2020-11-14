a7f39f089c5e0bc19ef2bfa749f2f014683c7129
kavitha333 <kavitha.xworkz.com>
2020-11-14 11:17:41 +0530

    Adding hospital java file

---
+++ hospital.java
@@ -0,0 +1,11 @@
+class Hospital
+{
+	static String area="OMR";
+	static String name="Nimance";
+	
+	public static void main(String a[])
+		{
+			System.out.println(area);
+			System.out.println(name);
+		}
+}
