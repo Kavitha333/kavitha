eac8199e3963c8389af391c2db4fa56dae9ec53e
kavitha333 <kavitha.xworkz.com>
2020-11-14 11:36:35 +0530

    adding diwali diwali

---
+++ Diwali.java
@@ -0,0 +1,27 @@
+class Diwali
+	{
+	public static void main(String[] aa)
+	{
+	pooja();
+	burningCrackers();
+	makingSweetsAtHome();	
+
+
+	}
+	
+	static void pooja()
+	{
+	System.out.println("Pooja @ 6");
+	}
+	
+	 static void burningCrackers()
+	{
+	System.out.println("Happy Diwali");
+	}
+	
+	static void makingSweetsAtHome()
+	{
+	System.out.println("Making Golab jamun");
+	}
+	
+}
